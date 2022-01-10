5ad32e2c055c6ee9124912ceaec85bd166bfd95d
Mohamad Rashad <mrashad@mrashad.com>
2022-01-10 19:10:04 +0300

    Rename the script1

--- script1_getRepo.sh
+++ script1_getRepo.sh
@@ -3,6 +3,6 @@
 git clone --recurse-submodules https://github.com/mrashad10/-admissiongitops.git gitOps
 cd gitOps
 git submodules update --remote
-./script2_build_images.sh
+./script2_start_service.sh
 
 docker stack deploy -c docker-compose.yml admission
