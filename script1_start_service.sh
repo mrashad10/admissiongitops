1452a01e2edea2425114aec87b6bbd9463f8eb41
Mohamad Rashad <mrashad@mrashad.com>
2022-01-14 15:59:13 +0300

    Fix docker swarm issue

--- script1_start_service.sh
+++ script1_start_service.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+function containerRestart {
+    badContainer=$(docker ps|grep admission_$1|grep -v "$1:latest"|cut -d' ' -f1)
+    if [ "$badContainer" != '' ];then
+        docker stop $badContainer
+    fi
+}
+
 
 git submodule update --remote
 
 docker build -t admissionapi:latest api
 docker build -t admissiondb:latest db
 
-docker stack deploy -c docker-compose.yml admission
-
-docker service update --force admission_db
-docker service update --force admission_api
+stackStatus=$(docker stack ls |grep admission|wc -l)
+if [ $stackStatus == 0 ];then
+    docker stack deploy -c docker-compose.yml admission
+else
+    containerRestart "admissionapi"
+    containerRestart "admissiondb"
+fi
